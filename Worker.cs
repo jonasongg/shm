6fd1f2bfa83e6d5d01c29bc0702c1e44c93d4596
Jonas Ong <jonasong2@gmail.com>
2025-05-15 14:47:19 +0800

    add CPU usage tracking

--- Worker.cs
+++ Worker.cs
@@ -16,16 +16,19 @@ public class Worker(ILogger<Worker> logger) : BackgroundService
                 throw new PlatformNotSupportedException("This service only works on Windows");
             }
 
-            using (var searcher = new ManagementObjectSearcher("select * from Win32_OperatingSystem"))
+            using (var memSearcher = new ManagementObjectSearcher("select * from Win32_OperatingSystem"))
+            using (var cpuSearcher = new ManagementObjectSearcher("select * from Win32_Processor"))
             {
                 // there's only one
-                var obj = searcher.Get().OfType<ManagementObject>().First();
+                var memObj = memSearcher.Get().OfType<ManagementObject>().First();
+                var cpuObj = cpuSearcher.Get().OfType<ManagementObject>().First();
 
-                var totalMemory = Convert.ToDouble(obj["TotalVisibleMemorySize"]);
-                var freeMemory = Convert.ToDouble(obj["FreePhysicalMemory"]);
+                var totalMemory = Convert.ToDouble(memObj["TotalVisibleMemorySize"]);
+                var freeMemory = Convert.ToDouble(memObj["FreePhysicalMemory"]);
                 var memoryUsagePercent = (totalMemory - freeMemory) / totalMemory * 100;
+                var cpuUsagePercent = Convert.ToDouble(cpuObj["LoadPercentage"]);
 
-                var info = new { totalMemory, memoryUsagePercent };
+                var info = new Report(totalMemory, memoryUsagePercent, cpuUsagePercent);
 
                 try
                 {
@@ -34,7 +37,6 @@ public class Worker(ILogger<Worker> logger) : BackgroundService
                 catch
                 {
                     logger.LogCritical("Couldn't post info");
-                    throw;
                 }
 
                 if (logger.IsEnabled(LogLevel.Information))
@@ -42,7 +44,9 @@ public class Worker(ILogger<Worker> logger) : BackgroundService
                     logger.LogInformation("Status: {info}", info);
                 }
             }
-            await Task.Delay(1000, stoppingToken);
+            await Task.Delay(5000, stoppingToken);
         }
     }
 }
+
+record Report(double TotalMemory, double MemoryUsagePercent, double CpuUsagePercent);
