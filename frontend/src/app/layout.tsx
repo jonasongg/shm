8caa5aece287dce335abe687adb0223b853179eb
Jonas Ong <jonasong2@gmail.com>
2025-05-22 11:58:44 +0800

    remove unnecessay frontend, begin template

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -23,9 +23,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="h-screen">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col h-full`}
       >
         {children}
       </body>
