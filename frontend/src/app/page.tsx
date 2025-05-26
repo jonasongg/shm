4d49bebe7a1823fd723fc1b1d9af8ec9a09eebd2
jonasongg <jonasong2@gmail.com>
2025-05-26 15:08:10 +0800

    frontend: add no active VM message

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,12 +6,17 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [data, setData] =
-    useState<Record<string, DataReportForVm[] | undefined>>();
+  const [data, setData] = useState<Partial<Record<string, DataReportForVm[]>>>(
+    {},
+  );
 
   useEffect(() => {
     (async () => {
       const response = await fetch("http://localhost:5043/reports");
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
+      }
+
       const json: DataReport[] = await response.json();
       const transformedData = json.map((data) => ({
         ...data,
@@ -26,31 +31,34 @@ export default function Home() {
       <header className="font-(family-name:--font-geist-sans) text-2xl font-extrabold bg-white p-6 border-b-1 centred-shadow">
         Dashboard
       </header>
-      <main className="p-8 gap-8 grid grid-cols-2 grid-rows-2 flex-1">
-        {data &&
-          [...Object.entries(data), ...Object.entries(data)].map(
-            ([name, reportForVm], i) => (
-              <Card key={i} className="font-(family-name:--font-geist-sans)">
-                <CardHeader className="text-xl">
-                  <CardTitle>{name}</CardTitle>
-                </CardHeader>
-                <CardContent className="flex flex-col h-full">
-                  {reportForVm ? (
-                    <CpuChart data={reportForVm} className="flex-3/4" />
-                  ) : (
-                    "No CPU data found."
-                  )}
-                  <hr className="border-b-1 my-3" />
-                  {reportForVm ? (
-                    <MemChart data={reportForVm} className="flex-1/4" />
-                  ) : (
-                    "No memory data found."
-                  )}
-                </CardContent>
-              </Card>
-            ),
-          )}
-      </main>
+      {Object.entries(data).length === 0 ? (
+        <div className="h-full flex items-center justify-center text-xl text-gray-500">
+          No active VMs to display
+        </div>
+      ) : (
+        <main className="p-8 gap-8 grid grid-cols-2 grid-rows-2 flex-1 font-(family-name:--font-geist-sans)">
+          {Object.entries(data).map(([name, reportForVm], i) => (
+            <Card key={i}>
+              <CardHeader className="text-xl">
+                <CardTitle>{name}</CardTitle>
+              </CardHeader>
+              <CardContent className="flex flex-col h-full">
+                {reportForVm ? (
+                  <CpuChart data={reportForVm} className="flex-3/4" />
+                ) : (
+                  "No CPU data found."
+                )}
+                <hr className="border-b-1 my-3" />
+                {reportForVm ? (
+                  <MemChart data={reportForVm} className="flex-1/4" />
+                ) : (
+                  "No memory data found."
+                )}
+              </CardContent>
+            </Card>
+          ))}
+        </main>
+      )}
     </>
   );
 }
