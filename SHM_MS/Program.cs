067e152d485fe5e52b2fcf2ee588ccc7c09acc18
Jonas Ong <jonasong2@gmail.com>
2025-05-28 14:30:39 +0800

    SHM: add SSE endpoint

--- SHM_MS/Program.cs
+++ SHM_MS/Program.cs
@@ -1,11 +1,15 @@
+using System.Text.Json;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Net.Http.Headers;
 using Shared.Models;
+using SHM_MS.Services;
 
 var AllowedSpecificOrigins = "_allowedSpecificOrigins";
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ReportContext>(options => options.UseInMemoryDatabase("ReportDb"));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddSingleton<ConsumerService>();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(
@@ -39,6 +43,29 @@ app.MapGet(
             .OrderByDescending(r => r.Timestamp)
             .GroupBy(r => r.Name)
             .SelectMany(rs => rs.Take(10));
+    }
+);
+
+app.MapGet(
+    "/reports/stream",
+    async (HttpContext ctx, ConsumerService consumerService, CancellationToken cancel) =>
+    {
+        ctx.Response.Headers.Append(HeaderNames.ContentType, "text/event-stream");
+        while (!cancel.IsCancellationRequested)
+        {
+            var report = await consumerService.WaitForReportAsync(cancel);
+
+            await ctx.Response.WriteAsync("data: ", cancel);
+            await JsonSerializer.SerializeAsync(
+                ctx.Response.Body,
+                report,
+                cancellationToken: cancel
+            );
+            await ctx.Response.WriteAsync("\n\n", cancel);
+            await ctx.Response.Body.FlushAsync(cancel);
+
+            consumerService.ResetTcs();
+        }
     }
 );
 
