cae4ecf1444522c7b3f264b17da48b6900370d1b
Jonas Ong <jonasong2@gmail.com>
2025-06-10 10:54:28 +0800

    SHM: add data retention policy to db

---
+++ SHM_MS/Migrations/20250610024800_AddDataRetentionPolicy.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SHM_MS.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDataRetentionPolicy : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("SELECT add_retention_policy('reports', INTERVAL '24 hours');");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("SELECT remove_retention_policy('reports');");
+        }
+    }
+}
