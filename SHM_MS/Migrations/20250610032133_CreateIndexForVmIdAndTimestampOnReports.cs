d9f57ea638e8d1ac86d1cfd66ba1d08aa604c0ba
Jonas Ong <jonasong2@gmail.com>
2025-06-10 11:28:49 +0800

    SHM: add index for vmId and time

---
+++ SHM_MS/Migrations/20250610032133_CreateIndexForVmIdAndTimestampOnReports.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SHM_MS.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateIndexForVmIdAndTimestampOnReports : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "ix_reports_vm_id_timestamp",
+                table: "reports",
+                columns: ["vm_id", "timestamp"],
+                descending: [false, true]
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(name: "ix_reports_vm_id_timestamp", table: "reports");
+        }
+    }
+}
