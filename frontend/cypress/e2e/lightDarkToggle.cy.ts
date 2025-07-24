b0f059c29161134444f1f934bea17544cb6862ff
Jonas Ong <jonasong2@gmail.com>
2025-07-24 15:28:01 +0800

    frontend test: add mode toggle test

---
+++ frontend/cypress/e2e/lightDarkToggle.cy.ts
@@ -0,0 +1,22 @@
+describe("light/dark toggle", () => {
+  beforeEach(() => {
+    cy.get("main>div").should("have.class", "grid-stack-static");
+  });
+
+  it("defaults to light mode", () => {
+    cy.contains("Toggle theme").find(".lucide-sun").should("be.visible");
+    cy.contains("Toggle theme").find(".lucide-moon").should("not.be.visible");
+  });
+
+  it("toggles between light and dark mode", () => {
+    cy.contains("Toggle theme").click();
+
+    cy.contains("Toggle theme").find(".lucide-sun").should("not.be.visible");
+    cy.contains("Toggle theme").find(".lucide-moon").should("be.visible");
+
+    cy.contains("Toggle theme").click();
+
+    cy.contains("Toggle theme").find(".lucide-sun").should("be.visible");
+    cy.contains("Toggle theme").find(".lucide-moon").should("not.be.visible");
+  });
+});
